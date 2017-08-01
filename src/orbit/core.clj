7e64c5dc5c1e95f3eebf1d4c07592b614571d5b0
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-08-02 08:46:50 +0900

    that file was a mistake somehow

--- src/orbit/core.clj
+++ src/orbit/core.clj
@@ -1,6 +1,7 @@
 (ns orbit.core
   "Calculating orbits by graph search algorithms."
-  (:require [clojure.core.reducers :as r])
+  (:require [orbit.extension :as ext]
+            [clojure.core.reducers :as r])
   (:gen-class))
 
 (declare single-step
@@ -27,32 +28,6 @@
   (load-file (first args))
   (shutdown-agents))
 
-;; dynamic variable for the size of the task in parallel execution
-(def ^:dynamic *task-size* 256)
-
-;; EXTENSION STRATEGIES
-(defn cf
-  ([] #{})
-  ([coll x] (into coll x)))
-
-(defn parallel-step
-  "Applies action to all elements in parallel using reducers.
-  It has to turn elts into a vector, otherwise fold does not  kick in."
-  [elts action]
-  [(r/fold *task-size* cf conj (r/mapcat action (vec elts))) #{}])
-
-(defn bulk-step
-  "Applies action to all elements in one go. Returns the empty set as
-  unprocessed elements."
-  [elts action]
-  [(r/reduce conj #{} (r/mapcat action elts)) #{}])
-
-(defn single-step
-  "Produces elements by applying the set-valued action to a single element
-  from the given collection of elements. Also returns unprocessed elements."
-  [elts action]
-  [(action (first elts)) (rest elts)])
-
 ; FULL ORBIT ALGORITHMS
 (defn full-orbit
   "Generic graph-search for producing the full orbit from seeds
@@ -70,7 +45,7 @@
 
 (defn full-orbit-parallel
   [seeds sa]
-  (full-orbit seeds sa parallel-step))
+  (full-orbit seeds sa ext/parallel-step))
 
 
 ;; seeds - elements to act on
@@ -79,13 +54,13 @@
   "Bulk-extension search starting from the elements in seeds using a single
   set-valued action function producing new elements."
   [seeds sa]
-  (full-orbit seeds sa bulk-step))
+  (full-orbit seeds sa ext/bulk-step))
 
 (defn full-orbit-single
   "Single extension search starting from the elements in seeds using a single
   set-valued action function."
   [seeds sa]
-  (full-orbit seeds sa single-step))
+  (full-orbit seeds sa ext/single-step))
 
 ; PARTIAL ORBITS, STOPPING AT FIRST SOLUTIONS
 (defn first-solution
@@ -105,12 +80,12 @@
 (defn first-solution-single
   "Returns a first solution when searching by breadth-first."
   [seed sa candidate? solution?]
-  (first-solution seed sa candidate? solution? single-step))
+  (first-solution seed sa candidate? solution? ext/single-step))
 
 (defn first-solution-bulk
   "Returns a first solution when searching by depth-first."
   [seed sa candidate? solution?]
-  (first-solution seed sa candidate? solution? bulk-step))
+  (first-solution seed sa candidate? solution? ext/bulk-step))
 
 (defn acyclic-search
   "Searching for solutions by predicate solution?, where the search graph is
@@ -125,8 +100,8 @@
 
 (defn acyclic-search-bulk
   [seeds sa solution?]
-  (acyclic-search seeds sa solution? bulk-step))
+  (acyclic-search seeds sa solution? ext/bulk-step))
 
 (defn acyclic-search-single
   [seeds sa solution?]
-  (acyclic-search seeds sa solution? single-step))
+  (acyclic-search seeds sa solution? ext/single-step))
--- src/orbit/extension.clj
+++ src/orbit/extension.clj
@@ -3,8 +3,11 @@
   an action."
   (:require [clojure.core.reducers :as r]))
 
+;; dynamic variable for the size of the task in parallel execution
+(def ^:dynamic *task-size* 256)
+
 ;; EXTENSION STRATEGIES
-(defn combine-sets
+(defn cf
   ([] #{})
   ([coll x] (into coll x)))
 
@@ -12,13 +15,13 @@
   "Applies action to all elements in parallel using reducers.
   It has to turn elts into a vector, otherwise fold does not  kick in."
   [elts action]
-  [(r/fold combine-sets conj (r/mapcat action (vec elts))) #{}])
+  [(r/fold *task-size* cf conj (r/mapcat action (vec elts))) #{}])
 
 (defn bulk-step
   "Applies action to all elements in one go. Returns the empty set as
   unprocessed elements."
   [elts action]
-  [(distinct (mapcat action elts)) #{}])
+  [(r/reduce conj #{} (r/mapcat action elts)) #{}])
 
 (defn single-step
   "Produces elements by applying the set-valued action to a single element
