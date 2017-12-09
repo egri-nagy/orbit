5b25bc60552a1e096b25c1dcc5f7ebf47b064cad
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-12-09 13:09:33 +0900

    clj, lein-ancient update

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://egri-nagy.github.io/orbit/"
   :license {:name "GNU GENERAL PUBLIC LICENSE"
             :url "http://www.gnu.org/copyleft/gpl.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [criterium "0.4.4"]
                  [com.taoensso/timbre "4.10.0"]
                  [slamhound "1.5.5"]]
   :plugins [[lein-cloverage "1.0.6"]
             [lein-kibit "0.1.2"]
-            [lein-ancient "0.6.10"]
+            [lein-ancient "0.6.14"]
             [lein-bikeshed "0.3.0"]
             [jonase/eastwood "0.2.3"]]
   :main ^:skip-aot orbit.core
