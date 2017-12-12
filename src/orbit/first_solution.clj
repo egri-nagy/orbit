d982dd302cba0f35197593c41aeec6739aa0b475
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-12-12 11:25:08 +0900

    added explanation for predicates

--- src/orbit/first_solution.clj
+++ src/orbit/first_solution.clj
@@ -3,7 +3,11 @@
 
 (defn first-solution
   "Generic search with the ability to bail out early when
-  a solution is found. It returns a solution or nil."
+  a solution is found. It returns a solution or nil.
+  candidate? returns true if the given element is a possible solution,
+  so even if it's not a solution, it is worth investigating.
+  solution? returns true when the given element is a solution.
+  Bulk and parallel extension strategies (stepf) may be wasteful."
   [seed sa candidate? solution? stepf]
   (loop [waiting (set [seed]), orbit #{}]
     (let [candidates (filter candidate? waiting)
