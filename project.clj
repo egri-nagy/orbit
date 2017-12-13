4d9fb6347071a981478803089f39842c13aa9782
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-12-13 10:15:11 +0900

    proper release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject orbit "17.12.12-SNAPSHOT"
+(defproject orbit "17.12.13"
   :description "Parallel orbit and search algorithms"
   :url "https://egri-nagy.github.io/orbit/"
   :license {:name "GNU GENERAL PUBLIC LICENSE"
