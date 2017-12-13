eb8b8e5a487652c4d1bccb6f9b09e24c3cbcadf5
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-12-13 09:58:59 +0900

    renaming test

--- test/orbit/first_solution_test.clj
+++ test/orbit/first_solution_test.clj
@@ -1,13 +1,12 @@
-(ns orbit.core-test
+(ns orbit.first-solution-test
   (:require [clojure.test :refer :all]
             [orbit.core :as orbit]))
 
-(deftest test-first-solution-*fs
+(deftest test-first-solution
   (let [f (fn [x] (hash-set (conj x (inc (count x)))))
         cand? (fn [x] (<= (count x) 11))
         sol? (fn [x] (= (count x) 11))
         bulksol (orbit/first-solution-bulk #{0} f cand? sol?)
-        singlesol (orbit/first-solution-single #{0} f cand? sol?)
-        ]
+        singlesol (orbit/first-solution-single #{0} f cand? sol?)]
     (testing "Testing bulk and single extension first solutions."
       (is (not (some nil? [bulksol singlesol]))))))
