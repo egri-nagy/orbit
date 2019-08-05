a7674b45052a8961f4eb621ec07638e6e4255716
Attila Egri-Nagy <attila@egri-nagy.hu>
2019-08-05 13:41:38 +0900

    downgrading kibit

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject orbit "18.06.14"
+(defproject orbit "19.08.05"
   :description "Parallel orbit and search algorithms"
   :url "https://egri-nagy.github.io/orbit/"
   :license {:name "GNU GENERAL PUBLIC LICENSE"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [slamhound "1.5.5"]]
   :plugins [[lein-cloverage "1.1.1"]
-            [lein-kibit "0.1.7"]
+            [lein-kibit "0.1.6"]
             [lein-ancient "0.6.15"]
             [lein-bikeshed "0.5.2"]
             [jonase/eastwood "0.3.6"]]
