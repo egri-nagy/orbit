6c916183e54aa0456fa96ee74ead97ca5bf66382
Attila Egri-Nagy <attila@egri-nagy.hu>
2022-04-15 16:56:09 +0900

    improving comments in the extension namespace

--- src/orbit/extension.clj
+++ src/orbit/extension.clj
@@ -1,8 +1,15 @@
 (ns orbit.extension
-  "Strategies for creating new elements (extensions) based on a set and
-  an action. General form: taking a collection and an action, returning
-  a new set of elements and the unprocessed, in a vector.
-  In graph terminology, we find the neighbouring nodes of nodes."
+  "When creating the orbit there are different ways for extending the orbit
+  being built. The general form of extension is to take a collection and an
+  action. Then based on applying the action we produce a new set of elements
+  and also return the unprocessed ones in a vector. This gives control on the
+  extension process. How to this exactly depends on the answers for the
+  following guiding questions.
+  1. Do we have an operator that produces a set of results, or just a single
+  one. SET-VALUED vs SINGLE VALUE
+  2. Do we need the extension happen one-by-one, or is it ok to extend the whole
+  frontline at once? single-step vs. parallel
+  3. Does the extensions happen in parallel or in a single thread?"
   (:require [clojure.core.reducers :as r]))
 
 ;; dynamic variable for the size of the task in parallel execution
@@ -17,17 +24,24 @@
 
 (defn parallel-step
   "Applies action to all elements in parallel using reducers.
-  It has to turn elts into a vector, otherwise fold does not  kick in.
+  It has to turn elts into a vector, otherwise fold does not kick in.
   Processes all elements, thus it returns the empty set unprocessed elements."
   [elts action]
-  [(r/fold *task-size* combine-function into (r/map action (vec elts)))
+  [(r/fold
+    *task-size*
+    combine-function
+    into
+    (r/map action (vec elts)))
    #{}])
 
 (defn bulk-step
   "Applies action to all elements in one go. Returns the empty set as
   unprocessed elements."
   [elts action]
-  [(r/reduce into #{} (r/map action elts))
+  [(r/reduce
+    into
+    #{}
+    (r/map action elts))
    #{}])
 
 (defn single-step
@@ -41,4 +55,8 @@
 (defn single-op-bulk-step
   "Produces elements by applying the single-valued operation to all elements."
   [elts action]
-  [(r/reduce conj #{} (r/map action elts))  #{}])
+  [(r/reduce
+    conj
+    #{}
+    (r/map action elts))
+   #{}])
