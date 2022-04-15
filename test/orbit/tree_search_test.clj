5400c64ca695b83c66ba70ddb8c65853e1820520
Attila Egri-Nagy <attila@egri-nagy.hu>
2022-04-15 17:28:44 +0900

    improving test coverage by adding parallel tree search test

--- test/orbit/tree_search_test.clj
+++ test/orbit/tree_search_test.clj
@@ -3,10 +3,20 @@
             [orbit.core :as orbit]))
 
 (deftest test-tree-search
-  (let [f (fn [x] (if (< (count x) 8)
+  (let [f (fn [x] (if (< (count x) 8) ;generating all bitstrings of length 8
                     #{(conj x 0) (conj x 1)}
                     #{})) ; generation supposed to stop when no more solutions
         sol? (fn [x] (= (count x) 8))
         sols (orbit/tree-search #{[]} f  sol?)]
     (testing "Testing tree search."
       (is (= 256 (count sols))))))
+
+(deftest test-ptree-search
+  (let [n 17
+        f (fn [x] (if (< (count x) n) ;generating all bitstrings of length 8
+                    #{(conj x 0) (conj x 1)}
+                    #{})) ; generation supposed to stop when no more solutions
+        sol? (fn [x] (= (count x) n))
+        sols (orbit/ptree-search #{[]} f  sol?)]
+    (testing "Testing tree search."
+      (is (== (Math/pow 2 n) (count sols))))))
