f5e3253a49de36de4d4a4cc03e96fc5682f314a1
Attila Egri-Nagy <attila@egri-nagy.hu>
2024-07-13 19:04:59 +0200

    updating deps

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://egri-nagy.github.io/orbit/"
   :license {:name "GNU GENERAL PUBLIC LICENSE"
             :url "http://www.gnu.org/copyleft/gpl.html"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.11.3"]
                  [criterium "0.4.6"]]
-  :plugins [[lein-cloverage "1.2.3"]
-            [lein-kibit "0.1.8"]
+  :plugins [[lein-cloverage "1.2.4"]
+            [lein-kibit "0.1.11"]
             [lein-ancient "0.7.0"]
             [lein-bikeshed "0.5.2"]
-            [jonase/eastwood "1.2.3"]]
+            [jonase/eastwood "1.4.3"]]
   :main ^:skip-aot orbit.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
