8bef23f4f06358d56e86e3eb63e49e0bbf06c329
Attila Egri-Nagy <attila@egri-nagy.hu>
2024-11-24 11:34:01 +0900

    revising documentation for extensions

--- src/orbit/extension.clj
+++ src/orbit/extension.clj
@@ -1,21 +1,23 @@
 (ns orbit.extension
-  "When creating the orbit there are different ways for extending the orbit
-  being built. The general form of extension is to take a collection and an
-  action. Then based on applying the action we produce a new set of elements
-  and also return the unprocessed ones in a vector. This gives control on the
-  extension process. How to this exactly depends on the answers for the
+  "There are different ways for extending the orbit being built.
+  The general form of extension is to take a collection (frontier) and an action.
+  By applying the action, we produce a new set of elements. In addition,
+  we also return the unprocessed elements in a vector. This gives control on the
+  extension process. How to do this exactly depends on the answers for the
   following guiding questions.
   1. Do we have an operator that produces a set of results, or just a single
-  one. SET-VALUED vs SINGLE VALUE
+  one? SET-VALUED vs SINGLE-VALUE
   2. Do we need the extension happen one-by-one, or is it ok to extend the whole
-  frontline at once? single-step vs. parallel
+  frontline at once? single-step vs bulk
   3. Does the extensions happen in parallel or in a single thread?"
   (:require [clojure.core.reducers :as r]))
 
 ;; dynamic variable for the size of the task in parallel execution
 (def ^:dynamic *task-size* 256)
 
-;; EXTENSION STRATEGIES - SET-VALUED ACTIONS
+;;; EXTENSION STRATEGIES
+
+;;SET-VALUED ACTIONS
 (defn combine-function
   "Combines intermediate results into a set.
   Required by fold in the reducers library."
@@ -51,7 +53,7 @@
   [(action (first elts))
    (rest elts)])
 
-;; SINGLE VALUE OPERATORS
+;; SINGLE-VALUE OPERATORS
 (defn single-op-bulk-step
   "Produces elements by applying the single-valued operation to all elements."
   [elts action]
